2f42053077cc63ead1c9be0c38be041fa22ef573
Mohammad <m7md389@gmail.com>
2022-01-08 18:01:27 +0200

    search on press Endter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import "./../styles/search-bar.css";
 
 function SearchBar({ searchValue, onChange, onSearch }) {
+  const handleKeyPress = function (event) {
+    const keyCode = event.code;
+    if (keyCode === "Enter") {
+      onSearch();
+    }
+  };
+
   return (
     <div className="search-bar">
       <input
@@ -9,6 +16,7 @@ function SearchBar({ searchValue, onChange, onSearch }) {
         placeholder="What do you want to search for?"
         value={searchValue}
         onChange={onChange}
+        onKeyPress={handleKeyPress}
       />
       <button onClick={onSearch}>Search</button>
     </div>
